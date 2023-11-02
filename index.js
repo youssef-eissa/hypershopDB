1fd5cad02f5cf2f7dcf2bebfed2046591d6d63a4
youssef-eissa <youssefeissa1812@gmail.com>
2023-11-02 21:27:33 +0200

    users DB

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+require('dotenv').config()
+const port =process.env.PORT ||8000
+const { readdirSync } = require('fs')
+app.get('/', (req, res) => {
+  res.send('main page')
+
+})
+readdirSync("./routes").map((file)=>app.use("/",require(`./routes/${file}`)))
+
+
+
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`)
+})
---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const usersData = [{
+        id: 1,
+      name: "youssef eissa",
+      username: "aaaaa",
+      email: "aa@ds.commm",
+      password: "11111111",
+      carts: [
+        {
+        id: 1,
+        title: "iPhone 9",
+        description: "An apple mobile which is nothing like apple",
+        price: 549,
+        discountPercentage: 12.96,
+        rating: 4.69,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/1/1.jpg",
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+        }
+    ]
+    },]
+
+router.get('/users', (req, res) => {
+    res.send(usersData);
+})
+module.exports = router
